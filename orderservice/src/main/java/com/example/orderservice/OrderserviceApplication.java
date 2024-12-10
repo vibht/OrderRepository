ae6344db5b4bb07e16e3374e872129cd4a35fcdf
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-12-10 15:16:35 +0530

    configuration is change

--- orderservice/src/main/java/com/example/orderservice/OrderserviceApplication.java
+++ orderservice/src/main/java/com/example/orderservice/OrderserviceApplication.java
@@ -3,10 +3,12 @@ package com.example.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderserviceApplication {
 
 	public static void main(String[] args) {
